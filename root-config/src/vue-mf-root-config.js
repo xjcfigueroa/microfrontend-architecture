4af919e9b9ee94ebda3ebf3aa2d6cc8c8814d25e
xjcfigueroa@github.com <jfigueroa@JC-MacBook-Pro.local>
2020-05-18 10:06:12 -0500

    initial commit

---
+++ root-config/src/vue-mf-root-config.js
@@ -0,0 +1,21 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@vue-mf/navbar",
+  app: () => System.import("@vue-mf/navbar"),
+  activeWhen: "/",
+});
+
+registerApplication({
+  name: "@vue-mf/dogs-dashboard",
+  app: () => System.import("@vue-mf/dogs-dashboard"),
+  activeWhen: "/view-doggos",
+});
+
+registerApplication({
+  name: "@vue-mf/rate-dogs",
+  app: () => System.import("@vue-mf/rate-dogs"),
+  activeWhen: "/rate-doggos",
+});
+
+start();
---
+++ styleguide/src/vue-mf-styleguide.js
@@ -0,0 +1,5 @@
+import "./set-public-path";
+import "./global.css";
+
+// You can export Vue components from this file and import them into your microfrontends
+export { default as PageHeader } from "./component-library/page-header.vue";
